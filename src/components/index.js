807dcf00bc1dbbda9b4b877a3453ad5aace464b6
mallisk <malli.sk@yahoo.com>
2024-06-08 10:42:28 -0700

    resolved the issue

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,7 @@ import Experience from "./Experience";
 import Works from "./Works";
 import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
-import CanvasLoader from "./Loader";
+import Loader from "./Loader";
 
 
 export {
@@ -19,7 +19,7 @@ export {
   Works,
   Feedbacks,
   Contact,
-  CanvasLoader,
+  Loader,
   EarthCanvas, 
   BallCanvas, 
   ComputersCanvas, 
